039f04f8d6e6760c75cc604e6ebd20949eea0a1f
宇超 周 <Yuchao@mBook.local>
2014-12-26 16:52:06 +0800

    2014.12.26
    2014.12.26

--- mlclass-ex8-006/mlclass-ex8/cofiCostFunc.m
+++ mlclass-ex8-006/mlclass-ex8/cofiCostFunc.m
@@ -40,7 +40,11 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+J_temp = (X * Theta' - Y).^2;
+J = sum(sum(J_temp(R == 1)))/2 + lambda/2 .* sum(sum(Theta.^2)) + lambda/2 .* sum(sum(X.^2));
 
+X_grad = ((X * Theta' - Y) .* R) * Theta + lambda.*X;
+Theta_grad = ((X * Theta' - Y) .* R)' * X + lambda.*Theta;
 
 
 
--- mlclass-ex8-006/mlclass-ex8/estimateGaussian.m
+++ mlclass-ex8-006/mlclass-ex8/estimateGaussian.m
@@ -21,7 +21,9 @@ sigma2 = zeros(n, 1);
 %               should contain variance of the i-th feature.
 %
 
+mu = 1/m * sum(X);
 
+sigma2 = 1/m * sum((X - repmat(mu, m, 1)).^2);
 
 
 
